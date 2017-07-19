7f6e234b5f8d2705e424bddf6097537cf4397847
Mitchell van Wijngaarden <mitchell@kooding.nl>
2017-07-19 18:04:03 +0200

    forgot something

--- src/IPv4.php
+++ src/IPv4.php
@@ -5,7 +5,7 @@ final class IPv4 implements IPAddress {
     private $ipAddress;
 
     public function __construct(?string $ipAddress) {
-        if (filter_var($ipAddress, FILTER_VALIDATE_IP, ['flags' => FILTER_FLAG_IPV4])) {
+        if ( ! filter_var($ipAddress, FILTER_VALIDATE_IP, ['flags' => FILTER_FLAG_IPV4])) {
             throw IPAddressNonCompliantWithIPVersion::IPv4($ipAddress);
         }
         $this->ipAddress = $ipAddress;
